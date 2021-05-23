1b22db6e49529e58b929d41cf19b9f6d00d5cfd0
Ankit-akumar <as2262011@gmail.com>
2021-05-24 00:34:04 +0530

    Implemented Sign in and Sign up UI.

---
+++ app/src/main/java/com/example/chatapp/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.example.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatapp.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
