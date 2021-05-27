412b3405f0958502ea758c1d90119ecbcc420ed1
Ankit-akumar <as2262011@gmail.com>
2021-05-28 01:48:38 +0530

    Implemented signIn using email and password backend  and logout and added menu

--- app/src/main/java/com/example/chatapp/SignInActivity.java
+++ app/src/main/java/com/example/chatapp/SignInActivity.java
@@ -2,13 +2,69 @@ package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.chatapp.databinding.ActivitySignInBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class SignInActivity extends AppCompatActivity {
+
+    ActivitySignInBinding binding;
+    ProgressDialog progressDialog;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        // get firebase instance
+        auth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(SignInActivity.this);
+        progressDialog.setTitle("Logging In");
+        progressDialog.setMessage("Please wait while we login to your account");
+
+        // if user has already logged in then start with Main Activity
+        if (auth.getCurrentUser() != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        // login the user
+        binding.btnLogin.setOnClickListener(view -> {
+            // show progress dialog while logging in
+            progressDialog.show();
+            auth.signInWithEmailAndPassword(binding.etEmail.getText().toString(), binding.etPassword.getText().toString()).
+                    addOnCompleteListener(task -> {
+                        // close progress dialog when task is complete
+                        progressDialog.dismiss();
+
+                        // if task was successful then start Main Activity
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else { // else show exception
+                            Toast.makeText(SignInActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
+
+        // start Sign up Activity
+        binding.tvClickSignUp.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
--- app/src/main/java/com/example/chatapp/SignUpActivity.java
+++ app/src/main/java/com/example/chatapp/SignUpActivity.java
@@ -1,24 +1,22 @@
 package com.example.chatapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import com.example.chatapp.Models.User;
 import com.example.chatapp.databinding.ActivitySignUpBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Objects;
+
 public class SignUpActivity extends AppCompatActivity {
     ActivitySignUpBinding binding;
-    private FirebaseAuth mAuth;
+    private FirebaseAuth auth;
     private FirebaseDatabase database;
 
     @Override
@@ -26,8 +24,9 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
-        mAuth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
 
         // Show progress dialog while creating account
@@ -38,7 +37,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         binding.btnSignUp.setOnClickListener(view -> {
             progressDialog.show();
-            mAuth.createUserWithEmailAndPassword(binding.etEmail.getText().toString(), binding.etPassword.getText().toString()).
+            auth.createUserWithEmailAndPassword(binding.etEmail.getText().toString(), binding.etPassword.getText().toString()).
                     addOnCompleteListener(task -> {
                         // close dialog
                         progressDialog.dismiss();
@@ -59,6 +58,13 @@ public class SignUpActivity extends AppCompatActivity {
                             Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     });
+        });
+
+        // start Sign in activity
+        binding.tvClickLogIn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SignInActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
